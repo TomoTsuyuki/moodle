716acb9e32a8b8d5d0f39a1c021ffb5c857fdb5f
Tomo Tsuyuki <tomotsuyuki@catalyst-au.net>
2024-02-28 15:29:20 +1100

    MDL-78902 backup: fix restore random question.

--- backup/moodle2/tests/restore_stepslib_test.php
+++ backup/moodle2/tests/restore_stepslib_test.php
@@ -17,6 +17,7 @@
 namespace core_backup;
 
 use backup;
+use restore_controller;
 
 /**
  * Tests for Moodle 2 restore steplib classes.
@@ -140,5 +141,48 @@ class restore_stepslib_test extends \advanced_testcase {
             $this->assertEquals($originalsections[2]->$field, $restoredsections[2]->$field);
         }
 
+    }
+
+    /**
+     * Test for the section structure step included elements.
+     *
+     * @covers \restore_move_module_questions_categories::process_section
+     * @return void
+     */
+    public function test_restore_random_question(): void {
+        global $DB, $USER;
+
+        $this->resetAfterTest();
+        $this->setAdminUser();
+
+        $backupfile = 'test_random_question_39';
+
+        // Extract backup file.
+        $backupid = $backupfile;
+        $backuppath = make_backup_temp_directory($backupid);
+        get_file_packer('application/vnd.moodle.backup')->extract_to_pathname(
+            __DIR__ . "/fixtures/$backupfile.mbz", $backuppath);
+
+        // Restore the quiz activity in the backup to a new course.
+        $coursecat = self::getDataGenerator()->create_category();
+        $course = self::getDataGenerator()->create_course(['category' => $coursecat->id]);
+        $rc = new restore_controller($backupid, $course->id, backup::INTERACTIVE_NO,
+            backup::MODE_GENERAL, $USER->id, backup::TARGET_EXISTING_ADDING);
+        $this->assertTrue($rc->execute_precheck());
+        $rc->execute_plan();
+        $rc->destroy();
+
+        // Get information about the quiz activity and confirm the references are correct.
+        $modinfo = get_fast_modinfo($course->id);
+        $quizzes = array_values($modinfo->get_instances_of('quiz'));
+        $contextid = $quizzes[0]->context->id;
+        $qcats = $DB->get_records('question_categories', ['contextid' => $contextid], 'parent');
+        $this->assertEquals(['top', 'Default for Test MDL-78902 quiz'], array_column($qcats, 'name'));
+        $references = $DB->get_records('question_set_references', ['usingcontextid' => $contextid]);
+        foreach ($references as $reference) {
+            $filtercondition = json_decode($reference->filtercondition);
+            $this->assertEquals($reference->questionscontextid,
+                $qcats[$filtercondition->questioncategoryid]->contextid);
+        }
     }
 }
